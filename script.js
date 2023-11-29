2b640c4809336ca56336b9a58ff0d55aaadb5401
mcarol47 <147444894+mcarol47@users.noreply.github.com>
2023-11-29 13:46:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log 
+let name = "Carolina";
+console.log(name)
+let number = 18;
+console.log(number)
+let number_multiplicado = number * 7;
+console.log(number_multiplicado)
+let amigos = ["Maria", "Bruno", "Mariana"];
+let numbers = [1, 2, 3, 4, 5];
+console.log(amigos[0]);
+// maria
+console.log(amigos[1]);
+// bruno
+console.log(amigos[2]);
+// mariana
+let person = {
+    name: "Carolina",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person.name);
+  // Carolina
+  console.log(person.age);
+  // 18
+  console.log(person.Student);
+  // student
+  let person2 = {
+    name: "Maria",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person2.name);
+  // Maria
+  console.log(person2.age);
+  // 18
+  console.log(person2.Student);
+  // student
+  let person3  = {
+    name: "Bruno",
+    age: 17,
+    Student: "verdadeiro"
+  };
+  console.log(person3.name);
+  // Bruno
+  console.log(person3.age);
+  // 17
+  console.log(person3.Student);
+  // student
+  let person4 = {
+    name: "Mariana",
+    age: 19,
+    Student: "verdadeiro"
+  };
+  console.log(person4.name);
+  // Mariana
+  console.log(person4.age);
+  // 19
+  console.log(person4.Student);
+  // student
